368c3afb9912db8c1793b2dbefb0f07671684407
ladan taheri <taheriprogrammer110@gmail.com>
2023-01-25 16:02:42 +0330

    fix show all

--- components/products/Products.js
+++ components/products/Products.js
@@ -3,15 +3,42 @@ import Item from '../common/sectionSlider/card/Item';
 import styles from './Products.module.css';
 import { getAllCalendars } from './../../services/calendarService';
 import { getAllPens } from '../../services/penService';
+import { getAllWoodenArtifact } from './../../services/woodencalendar';
+import { getAllDeskCalendars } from './../../services/deskCalendarService';
 class Products extends Component {
     state = {
         calendars: [],
-        pens: []
+        pens: [],
+        desk_calenders:[],
+        wooden:[]
 
     }
     componentDidMount = () => {
             this.handleGetCalendars();
             this.handleGetPens();
+            this.handleGetWoodenArtifact();
+            this.handleGetDeskCalendar();
+          };
+          handleGetDeskCalendar = async () => {
+            console.log("desktop calendar");
+            try {
+              const { data, status } = await getAllDeskCalendars();
+              if (status === 200) {
+                this.setState({ desk_calenders: data });
+              }
+            } catch (err) {
+              console.error(err);
+            }
+          };
+          handleGetWoodenArtifact = async () => {
+            try {
+              const { data, status } = await getAllWoodenArtifact();
+              if (status === 200) {
+                this.setState({ wooden: data });
+              }
+            } catch (err) {
+              console.error(err);
+            }
           };
 
     handleGetCalendars = async () => {
@@ -55,7 +82,10 @@ class Products extends Component {
 
                     {this.props.title === "سالنامه" ?  this.state.calendars.map(m => <Item key={m.id} type='سالنامه' item={m} category='calendar'/>) :null}
                     {this.props.title === "قلم های نفیس Carteie" ? this.state.pens.map(m => <Item key={m.id} type='قلم نفیس' item={m}  category='pen'/>) : null}
-                </section>
+                    {this.props.title === "محصولات چوبی" ? this.state.wooden.map(m => <Item key={m.id} type='چوبی' item={m}  category='wooden'/>) : null}
+                    {this.props.title === "تقویم رومیزی"? this.state.desk_calenders.map(m => <Item key={m.id} type='رومیزی' item={m}  category='deskcalendar'/>) : null}
+               
+               </section>
               
             </section>
         );
