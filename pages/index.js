f3095b10034c1ca72bddac19b7e3914995cd2707
ladan taheri <taheriprogrammer110@gmail.com>
2023-01-27 16:02:55 +0330

    change site title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>اداک</title>
+        <title>سالنامه و هدایا اداک</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NewHome />
